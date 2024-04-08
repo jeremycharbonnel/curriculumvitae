5af59119904cc66d7db377c60267dab0511a70a7
Jeremy Charbonnel <jeremy.charbonn3l@gmail.com>
2024-04-08 11:40:11 +0200

    Added hero-pattern plugin & updated default colors configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,26 +10,31 @@ module.exports = {
 				200: '#434C5E',
 				300: '#3B4252',
 				400: '#2E3440',
+				DEFAULT: '#2E3440'
 			},
 			'white': {
 				100: '#ECEFF4',
 				200: '#E5E9F0',
 				300: '#D8DEE9',
+				DEFAULT: '#ECEFF4'
 			},
 			'blue': {
 				100: '#8FBCBB',
 				200: '#88C0D0',
 				300: '#81A1C1',
 				400: '#5E81AC',
+				DEFAULT: '#81A1C1'
 			},
 			purple: '#B48EAD',
 			green: '#A3BE8C',
 			yellow: '#EBCB8B',
 			orange: '#D08770',
-			red: '#BF616A',
+			red: '#BF616A'
 		},
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		require('tailwindcss-hero-patterns'),
+	],
 }
 
